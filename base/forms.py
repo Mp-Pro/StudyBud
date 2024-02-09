63a60eef54cbc0ecd53fb83468a8e88e6c41576f
Mp-Pro <mahipalsingh1891@gmail.com>
2024-02-09 23:58:10 +0530

    first commit

---
+++ base/forms.py
@@ -0,0 +1,16 @@
+from django.forms import ModelForm
+from . models import Room,User  # custom user model
+# from django.contrib.auth.models import User  #default user model
+from django.contrib.auth.forms import UserCreationForm
+
+
+class RoomForm(ModelForm):
+    class Meta:
+        model = Room
+        fields = '__all__'
+        exclude = ['host','participants']
+
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ['avatar','name','username','email','bio']
